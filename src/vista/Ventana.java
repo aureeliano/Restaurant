60caa182cc82d3943db78d7f041c028f6204f20e
Paula <paula.bonifazi.aquino@gmail.com>
2022-11-03 18:25:55 -0300

    Se modificó la calse ventana

--- src/vista/Ventana.java
+++ src/vista/Ventana.java
@@ -10,32 +10,15 @@ import javax.swing.border.EmptyBorder;
 public class Ventana extends JFrame {
 
 	private JPanel contentPane;
+	private static final long serialVersionUID = 48;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Ventana frame = new Ventana();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
+	
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		this.contentPane = new JPanel();
 		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		
 		setContentPane(this.contentPane);
 	}
 
