30e5bb62a1e5ab5bb334384ddea7006c0c035d8c
Paula <paula.bonifazi.aquino@gmail.com>
2022-11-09 12:03:27 -0300

    se agregaron las pestañas a la ventana principal

--- src/vista/Ventana.java
+++ src/vista/Ventana.java
@@ -5,10 +5,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
 
 public class Ventana extends JFrame {
 
 	private JPanel contentPane;
+	private JTabbedPane tabbedPane;
+	private JPanel panel_registro;
+	private JPanel panel_mesas;
+	private JPanel panel_mozos;
+	private JPanel panel_productos;
+	private JPanel panel_promociones;
+	private JScrollPane scrollPane_lista_mesas;
+	private JScrollPane scrollPane_lista_mozos;
+	private JScrollPane scrollPane_lista_promociones;
+	private JScrollPane scrollPane_lista_productos;
 
 	/**
 	 * Launch the application.
@@ -31,11 +44,46 @@ public class Ventana extends JFrame {
 	 */
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 576, 425);
 		this.contentPane = new JPanel();
 		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(this.contentPane);
+		this.contentPane.setLayout(new BorderLayout(0, 0));
+		
+		this.tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		this.contentPane.add(this.tabbedPane, BorderLayout.CENTER);
+		
+		this.panel_registro = new JPanel();
+		this.tabbedPane.addTab("Registro", null, this.panel_registro, null);
+		this.panel_registro.setLayout(new BorderLayout(0, 0));
+		
+		this.panel_mesas = new JPanel();
+		this.tabbedPane.addTab("Mesas", null, this.panel_mesas, null);
+		this.panel_mesas.setLayout(new BorderLayout(0, 0));
+		
+		this.scrollPane_lista_mesas = new JScrollPane();
+		this.panel_mesas.add(this.scrollPane_lista_mesas, BorderLayout.WEST);
+		
+		this.panel_mozos = new JPanel();
+		this.tabbedPane.addTab("Mozos", null, this.panel_mozos, null);
+		this.panel_mozos.setLayout(new BorderLayout(0, 0));
+		
+		this.scrollPane_lista_mozos = new JScrollPane();
+		this.panel_mozos.add(this.scrollPane_lista_mozos);
+		
+		this.panel_productos = new JPanel();
+		this.tabbedPane.addTab("Productos", null, this.panel_productos, null);
+		this.panel_productos.setLayout(new BorderLayout(0, 0));
+		
+		this.scrollPane_lista_productos = new JScrollPane();
+		this.panel_productos.add(this.scrollPane_lista_productos, BorderLayout.WEST);
+		
+		this.panel_promociones = new JPanel();
+		this.tabbedPane.addTab("Promociones", null, this.panel_promociones, null);
+		this.panel_promociones.setLayout(new BorderLayout(0, 0));
+		
+		this.scrollPane_lista_promociones = new JScrollPane();
+		this.panel_promociones.add(this.scrollPane_lista_promociones, BorderLayout.WEST);
 	}
-
 }
